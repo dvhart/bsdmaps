25f2a1f6ff6d7bca56071c175282143c4c08a5ae
Brad Larson <bhlarson@gmail.com>
2016-02-02 00:38:08 -0800

    Compute Accident rate per route
    Added capability to compute accident rate per route.  Safety.js and
    Safety.html allow accident data to be entered.  Recalculate.js  computes
    but does not yet add accident rate to per route to grid.

---
+++ js/ServerApi.js
@@ -0,0 +1,47 @@
+﻿
+// Load route polylines from Server
+// Example: /GetSection
+//$http.get('/GetSection').then(function (response) {
+//    routes = RefreshFromSafetyDB(response, map);
+//});
+
+// Example /NewSection
+//$http.post('/NewSection', section).then(function (response) {
+//    routes = RefreshFromSafetyDB(response, map);
+//});
+
+
+function RefreshFromSafetyDB(dbData, renderMap) {
+    var dbRoutes = []
+    try {
+        
+        dbRoutes = dbData.data;
+        
+        // Find max accident rate
+        var maxRate = 0;
+        dbRoutes.forEach(function (route) {
+            if (route.rate > maxRate) {
+                maxRate = route.rate;
+            }
+        });
+        
+        // Set visible with color based on heat map
+        dbRoutes.forEach(function (route) {
+            var color = HeatMap(0, maxRate, route.rate);
+            var polyline = new google.maps.Polyline({
+                path: route.polyline.j,
+                strokeColor: color,
+                strokeWeight: 2
+            });
+            route.polyline = polyline;
+            route.polyline.setMap(renderMap);
+        });
+        
+        return dbRoutes;
+        
+    } catch (error) {
+        console.log('error ' + error);
+        
+        return dbRoutes;
+    }
+};
